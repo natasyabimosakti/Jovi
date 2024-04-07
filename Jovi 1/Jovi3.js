59f92c02e0795407e98d3a59e29f924895b34c6d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-04-07 23:18:38 +0700

    Update Jovi3.js

--- Jovi 1/Jovi3.js
+++ Jovi 1/Jovi3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.12
+// @version      3.13
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi3.js
@@ -27,7 +27,7 @@ var namagroup2 = 'MERCON';
 var Comment2 = 'MERCONTOTO BY666HE 10*73*65';
 
 var namagroup3 = 'IND0SL0TER';
-var Comment3 = 'BPKKAU4 : 27*90';
+var Comment3 = 'BPKKAU4 : 27*90*76';
 
 var namagroup4 = 'BUPATI';
 var Comment4 = '#BUPATITOGEL (B2UIKB) = 52*69*51';
