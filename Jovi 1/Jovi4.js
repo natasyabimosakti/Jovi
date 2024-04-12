6106bf04e9855fa5e0603e3de5ddf09ad567cbc6
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-04-12 15:55:52 +0700

    Update Jovi4.js

--- Jovi 1/Jovi4.js
+++ Jovi 1/Jovi4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.13
+// @version      3.14
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
@@ -44,8 +44,8 @@ var Comment7 = 'AXU78A : 98*93*37 #HOHOTOGEL';
 var namagroup8 = 'KITAJITU';
 var Comment8 = '#KITAJITU / AN6B77K / 95*59*16 / #Betting';
 
-var namagroup9 = 'SHI0T0GEL';
-var Comment9 = '#Shiotogel4d (ADOHAY78) 28*75*96';
+var namagroup9 = 'KIU';
+var Comment9 = '#(KIUTOTO) (APAKDO7) : 28*75*96';
 
 var namagroup10 = 'SLOTOGEL';
 var Comment10 = '#SLOTOGEL (AI9O3K) : 78*57*25';
