47fc9ec7705aeadfec9e0c4001884fa17f465404
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-04-22 01:45:48 +0700

    Update Jovi3.js

--- Jovi 1/Jovi3.js
+++ Jovi 1/Jovi3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.16
+// @version      3.17
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi3.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi3.js
@@ -35,7 +35,7 @@ var Comment4 = '#BUPATITOGEL (B2UIKB) = 52*69*51';
 var namagroup5 = 'OTU';
 var Comment5 = '#OTUSLOT*BJL89KA*70*55*74*BETTING';
 
-var namagroup6 = 'Mantap Jitu';
+var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (BADJING30) = 36*56*82';
 
 var namagroup7 = 'Hoho';
