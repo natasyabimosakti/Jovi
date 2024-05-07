f70f846e25bf25d576f95966cdc83616365d12d5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-07 17:07:33 +0700

    Update Jovi4.js

--- Jovi 1/Jovi4.js
+++ Jovi 1/Jovi4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.22
+// @version      3.23
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
@@ -23,7 +23,7 @@ var refresh = 50;
 var namagroup1 = 'TAFSIR';
 var Comment1 = 'TAG4D : (ASYEMKE) 78*57*25';
 
-var namagroup2 = 'K86T0T0';
+var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( AISHH98 ) : 48*89*09';
 
 var namagroup3 = 'INDOSL0TER';
