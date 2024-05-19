8b29cedec14a805fd5ba6c675c1477339628d920
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-20 02:25:34 +0700

    Update Jovi1.js

--- Jovi 1/Jovi1.js
+++ Jovi 1/Jovi1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Jovi 1
 // @namespace    http://tampermonkey.net/
-// @version      3.00
+// @version      3.27
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js
