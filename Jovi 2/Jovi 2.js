5be1a838d6e2d28eb5b06f2d987a3c8fe079b6b1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-05-24 17:54:21 +0700

    Update Jovi 2.js

--- Jovi 2/Jovi 2.js
+++ Jovi 2/Jovi 2.js
@@ -19,16 +19,16 @@ var refresh = 60;
 
 /---------------------------------------------/
 
-var namagroup1 = 'TAFSIR';
+var namagroup1 = 'BUKU';
 var Comment1 = 'TAG4D : (TUKIMAN8) 44*14*59'; 
 
-var namagroup2 = 'K86T0T0';
+var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( TUTUKI9 ) : 19*81*69'; 
 
 var namagroup3 = 'PUSAT LOMBA';
 var Comment3 = 'Shiotogel4d (TAMPAR6) 56*61*73'; 
 
-var namagroup4 = 'SEJITU';
+var namagroup4 = 'SEJ1TU';
 var Comment4 = '#SEJITU ( TURISMAN3 ) : 94*41*00'; 
 
 var namagroup5 = 'RNR';
