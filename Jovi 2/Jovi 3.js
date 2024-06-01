c30540e7fb6ec28f2a8dbaec37432449cf552c85
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-01 11:25:46 +0700

    Update Jovi 3.js

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.03
+// @version      3.02
 // @description  try to take over the world!
 // @updateURL    https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%202/Jovi%203.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%202/Jovi%203.js
