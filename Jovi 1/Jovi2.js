c1f5701eccb419e59bb461974e4dfac1bbe055c5
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-06-06 19:05:44 +0700

    Update Jovi2.js

--- Jovi 1/Jovi2.js
+++ Jovi 1/Jovi2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 2
 // @namespace    http://tampermonkey.net/
-// @version      3.34
+// @version      3.35
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi2.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi2.js
@@ -385,6 +385,6 @@ function clicksend() {
         clicksendcoment.dispatchEvent (clickEvent);
         console.log("Comment Terkirim");
         location.href = "about:blank"
-        /Tekan TOMBOL SEND/
+        //Tekan TOMBOL SEND//
     }
 }
