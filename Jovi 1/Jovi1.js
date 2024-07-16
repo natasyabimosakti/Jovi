57b99ec5d0fe45a6d16e026c58b852289f98dc4b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-16 15:51:53 +0700

    Update Jovi1.js

--- Jovi 1/Jovi1.js
+++ Jovi 1/Jovi1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.44
+// @version      3.45
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js
