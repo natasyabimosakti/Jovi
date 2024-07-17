c01ab82f49194ad35576d43c15deec8d09eccb09
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-07-17 21:21:42 +0700

    Update Jovi 3.js

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -19,8 +19,8 @@
 
 /*======================================================================3copy tampermonkey==========================================================*/
 
-var namagroup1 = 'RITOGEL';
-var Comment1 = '#RITOGEL(JSIUH0)85*90*95';
+var namagroup1 = 'YAO';
+var Comment1 = '#YAOTOGEL JSIUH0 85*90*95';
 
 var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( JEHOASYU7 ) : 38*50*57'; 
@@ -106,7 +106,7 @@ var id17 = await GM.getValue(17);
 
 var refresh = 40;
 
-var admin = ["aldi","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","andy","ayesha",
+var admin = ' ',["aldi","artha","adelia","ananda","aditia","andri","ayesha","aurel","alde","adm","audi","andy","ayesha",
              "brian","boboho","bobby","bonar","bella","bastian","bungaps",
              "cinta","calvin","celsia","celine",
              "denis","dollar","dewa","dewi","dinda","dika","dea",
