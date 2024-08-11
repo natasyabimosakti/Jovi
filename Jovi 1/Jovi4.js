cb1643524576740c5bc073485eaefbb5277b25ec
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-08-12 01:48:53 +0700

    Update Jovi4.js

--- Jovi 1/Jovi4.js
+++ Jovi 1/Jovi4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.54
+// @version      3.55
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js
@@ -46,8 +46,8 @@ var Comment8 = 'Tok99Toto ( AYEREKZ8 ) : 16*59*95';
 var namagroup9 = 'OTU';
 var Comment9 = '#OTUSLOT*A8JKD329*28*75*96*BETTING'; 
 
-var namagroup10 = 'LING';
-var Comment10 = 'LING77 (BVNR45) 25*57';
+var namagroup10 = 'SLOTOGEL';
+var Comment10 = '#SLOTOGEL (AI9O3K) 25*57*78';
 
 var namagroup11 = 'KEBAYA';
 var Comment11 = '(KEBAYA4D) = (ANKCNDA96) 37*93*98';
