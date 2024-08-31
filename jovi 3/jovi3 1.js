2d3fb4bf31d4c42700a79407f502eafbff0a80e1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-01 00:24:46 +0700

    Update jovi3 1.js

--- jovi 3/jovi3 1.js
+++ jovi 3/jovi3 1.js
@@ -20,52 +20,52 @@
 /*======================================================================3copy tampermonkey==========================================================*/
 
 var namagroup1 = 'POMPA';
-var Comment1 = '#POMPA4D(LATAO9) : 62*01*41';
+var Comment1 = '#POMPA4D(LATAO9) : 01*41*62';
 
 var namagroup2 = 'K86';
-var Comment2 = 'K86TOTO ( MUKEL3 ) : 94*74*48';
+var Comment2 = 'K86TOTO ( LINLY8 ) : 48*74*94';
 
 var namagroup3 = 'PUSAT LOMBA';
-var Comment3 = 'Shiotogel4d (MMASU6) 52*36*51';
+var Comment3 = 'Shiotogel4d (LIKOP1) 36*51*52';
 
 var namagroup4 = 'TEMPO';
-var Comment4 = 'MMAKS37 : 47*59*31 #TEMPOTOTO';
+var Comment4 = 'LVIEN0 : 31*47*59 #TEMPOTOTO';
 
 var namagroup5 = 'RNR';
-var Comment5 = '‌#RNR303(MUKBER2) : 65*20*80';
+var Comment5 = '‌#RNR303(LKTDM8) : 20*65*80';
 
 var namagroup6 = 'DIVA4D';
-var Comment6 = '#DIVA4D (MKKU1) = 82*56*55';
+var Comment6 = '#DIVA4D (LLGLM6) = 55*56*82';
 
 var namagroup7 = 'ORMAS';
-var Comment7 = 'ORMASTOTO(MNB89) : 37*27*07';
+var Comment7 = 'ORMASTOTO(LMKMI4) : 07*27*37';
 
 var namagroup8 = 'INFO';
-var Comment8 = 'Tok99Toto ( MHGUA2 ) : 16*13*46';
+var Comment8 = 'Tok99Toto ( LOPXE2 ) : 13*16*46';
 
 var namagroup9 = 'OTU';
-var Comment9 = '#OTUSLOT*MUNROH6*94*74*48*BETTING';
+var Comment9 = '#OTUSLOT*LOLGS6*48*74*94*BETTING';
 
 var namagroup10 = 'LING';
-var Comment10 = 'LING77 (MEMX22) 47*59'; 
+var Comment10 = 'LING77 (LPECR3) 31*47'; 
 
 var namagroup11 = 'KEBAYA';
-var Comment11 = '(KEBAYA4D) = (MAUSS1) 62*01*41s';
+var Comment11 = '(KEBAYA4D) = (LBZTV2) 01*41*62';
 
 var namagroup12 = 'BESTOTO';
-var Comment12 = 'MOMMOK0 : 48*82*89 #BESTOTO88';
+var Comment12 = 'LJORT6 : 13*16*46 #BESTOTO88';
 
 var namagroup13 = 'GOHT0G3L';
-var Comment13 = 'GOHTOGEL=MADLEN5=12*18*78';
+var Comment13 = 'GOHTOGEL=LPAK3=55*56*82';
 
 var namagroup14 = 'BLITAR';
-var Comment14 = '#BLITAR4D ( MIRA95 ) : 17*23*49';
+var Comment14 = '#BLITAR4D ( LUPWI10 ) : 07*27*37';
 
 var namagroup15 = 'MAIN';
-var Comment15 = '#(MAINTOGEL365) (MJUKI3) : 05*83*92';
+var Comment15 = '#(MAINTOGEL365) (LVTRY8) : 20*65*80';
 
 var namagroup16 = 'KECERDASAN';
-var Comment16 = 'CITA4D*MLARATS7*01*39*67';
+var Comment16 = 'CITA4D*LQOTP3*36*51*52';
 
 /*======================================================================3copy tampermonkey==========================================================*/
 
