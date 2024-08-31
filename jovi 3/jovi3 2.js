75896924596e655c2a1e31099bd34d3440b08a67
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-01 00:05:28 +0700

    Update jovi3 2.js

--- jovi 3/jovi3 2.js
+++ jovi 3/jovi3 2.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         JOVI 1
+// @name         JOVI 2
 // @namespace    http://tampermonkey.net/
 // @version      3.00
 // @description  Script Metode Terbaru, Cari Semua Keyword
-// @updateURL	   https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/jovi%203/jovi3%202.js?token=GHSAT0AAAAAACVCFJDJRJEJRZPG3VQJ63QEZWTJVQA
+// @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/jovi%203/jovi3%202.js?token=GHSAT0AAAAAACVCFJDJRJEJRZPG3VQJ63QEZWTJVQA
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/jovi%203/jovi3%202.js?token=GHSAT0AAAAAACVCFJDJRJEJRZPG3VQJ63QEZWTJVQA
 // @author       You
 // @match        http*://*/*
