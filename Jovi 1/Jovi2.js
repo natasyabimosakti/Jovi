bbbfa54e2b6a400b566f170ea3f370778c8ea9ea
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-10 20:32:34 +0700

    Update Jovi2.js

--- Jovi 1/Jovi2.js
+++ Jovi 1/Jovi2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 2
 // @namespace    http://tampermonkey.net/
-// @version      3.64
+// @version      3.65
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi2.js?token=GHSAT0AAAAAACVCFJDIE3ACEDKEHZ5WBRSMZWV3MMQ
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi2.js?token=GHSAT0AAAAAACVCFJDIE3ACEDKEHZ5WBRSMZWV3MMQ
@@ -45,7 +45,7 @@ var namagroup8 = 'INFO';
 var Comment8 = 'Tok99Toto ( DDUKOL5 ) : 26*61*72';
 
 var namagroup9 = 'OTU';
-var Comment9 = '#OTUSLOT*DF892K*07*14*31*BETTING';
+var Comment9 = 'OTUSLOT*DF892K*07*14*31*BETTING';
 
 var namagroup10 = 'LING';
 var Comment10 = 'LING77 (DVDCL33) : 38*53';
