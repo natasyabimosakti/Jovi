aae158eeae29432bdb6e75244e35f78dbfb38ae4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-09-25 01:06:04 +0700

    Update Jovi4.js

--- Jovi 1/Jovi4.js
+++ Jovi 1/Jovi4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.70
+// @version      3.71
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js?token=GHSAT0AAAAAACVCFJDIQ6LH5GPAZWOZ3H42ZWV3OQA
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js?token=GHSAT0AAAAAACVCFJDIQ6LH5GPAZWOZ3H42ZWV3OQA
@@ -44,14 +44,14 @@ var Comment7 = 'ORMASTOTO(AXI20) : 37*93*98';
 var namagroup8 = 'INFO';
 var Comment8 = 'Tok99Toto ( AYEREKZ8 ) : 16*59*95';
 
-var namagroup9 = 'OTU';
-var Comment9 = 'OTUSLOT*A8JKD329*28*75*96*BETTING'; 
+var namagroup9 = 'CUYTOTO';
+var Comment9 = '#CUYTOTO*A8JKD329*28*75*96';
 
 var namagroup10 = 'LING';
 var Comment10 = 'LING77 (BVNR45) 25*57';
 
-var namagroup11 = 'KEBAYA';
-var Comment11 = '(KEBAYA4D) = (ANKCNDA96) 37*93*98';
+var namagroup11 = 'Pasar';
+var Comment11 = 'PAJAKTOTO(ANKCNDA96) : 37*93';
 
 var namagroup12 = 'NEMO4D';
 var Comment12 = 'NEMO4D (AKP3200) : 09*48*89';
