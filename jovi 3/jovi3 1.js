a1ce956c6942a0892f96a7cd5272c649b78063ab
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-02 05:23:53 +0700

    Update jovi3 1.js

--- jovi 3/jovi3 1.js
+++ jovi 3/jovi3 1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.13
+// @version      3.14
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/jovi%203/jovi3%201.js?token=GHSAT0AAAAAACVCFJDJT7B6YM63KWUTPFF4ZWTHLWA
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/jovi%203/jovi3%201.js?token=GHSAT0AAAAAACVCFJDJT7B6YM63KWUTPFF4ZWTHLWA
@@ -43,8 +43,8 @@ var Comment7 = 'ORMASTOTO(LMKMI4) : 07*27*37';
 var namagroup8 = 'INFO';
 var Comment8 = 'Tok99Toto ( LOPXE2 ) : 13*16*46';
 
-var namagroup9 = 'CUYTOTO';
-var Comment9 = '#CUYTOTO*LOLGS6*48*74*94';
+var namagroup9 = 'TOYIBSLOT';
+var Comment9 = '#TOYIBSLOT ( LOLGS6 ) : 48*74*94';
 
 var namagroup10 = 'LING';
 var Comment10 = 'LING77 (LPECR3) 31*47'; 
