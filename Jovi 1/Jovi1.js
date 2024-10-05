8ac75aaef4d19cb3702211b9c5094a69cb7cd12a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-10-05 07:37:28 +0700

    Update Jovi1.js

--- Jovi 1/Jovi1.js
+++ Jovi 1/Jovi1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.76
+// @version      3.77
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js?token=GHSAT0AAAAAACVCFJDJ2MSABLMJWRJLMETGZWV3FFQ
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi1.js?token=GHSAT0AAAAAACVCFJDJ2MSABLMJWRJLMETGZWV3FFQ
@@ -148,6 +148,7 @@ var Backlist4 = "room lomba freebet";
 var Backlist5 = "prediksi";
 var Backlist6 = "result";
 var Backlist7 = "result";
+var Backlist8 = "3D";
 
 var myrefresh = setInterval(function(){
   
