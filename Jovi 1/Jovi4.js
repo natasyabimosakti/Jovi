8c456e8f3cb3c2775471fad5b42da74c55464d55
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-06 13:36:25 +0700

    Update Jovi4.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 1/Jovi4.js
+++ Jovi 1/Jovi4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.93
+// @version      3.94
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js?token=GHSAT0AAAAAACVCFJDIQ6LH5GPAZWOZ3H42ZWV3OQA
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/main/Jovi%201/Jovi4.js?token=GHSAT0AAAAAACVCFJDIQ6LH5GPAZWOZ3H42ZWV3OQA
@@ -53,8 +53,8 @@ var Comment10 = 'LING77 (BVNR45) 25*57';
 var namagroup11 = 'Pasar';
 var Comment11 = 'PAJAKTOTO(ANKCNDA96) : 37*93';
 
-var namagroup12 = 'KINGDOM';
-var Comment12 = 'JANGKAUTOTO(AKP3200) : 09*48*89';
+var namagroup12 = 'KAGET';
+var Comment12 = 'DAGELAN4D(AKP3200) : 09*48';
 
 var namagroup13 = 'Wellz';
 var Comment13 = 'SIJITOGEL ALDVQ5 50*67*86';
