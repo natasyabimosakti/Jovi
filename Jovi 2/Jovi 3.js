c5cda25b369cd1acf64be2ef7888235f151db03f
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-09 02:21:57 +0700

    Update Jovi 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -4,7 +4,7 @@
 // @version      3.10
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
-// @downloadURL	https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
+// @downloadURL 	https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
 // @author       You
 // @match        http*://*/*
 // @run-at       document-end
