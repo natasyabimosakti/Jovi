bdca1304ea65e2057e72ca086b4936475c77214d
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-13 20:29:05 +0700

    Update Jovi 3.js

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.11
+// @version      3.12
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
@@ -50,7 +50,7 @@ var namagroup10 = 'LING';
 var Comment10 = 'LING77 (JDJS4) 85*90';
 
 var namagroup11 = 'TASIKTOTO';
-var Comment11 = '#TASIKTOTO(JOKIAD8) = 46*84
+var Comment11 = '#TASIKTOTO(JOKIAD8) = 46*84';
   
 var namagroup12 = 'KAGET';
 var Comment12 = 'DAGELAN4D(JELONN9) : 38*50';
