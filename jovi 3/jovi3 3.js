fa23cc535554bc832f12576e27ba45ea28f94b26
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-26 00:04:50 +0700

    Update jovi3 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- jovi 3/jovi3 3.js
+++ jovi 3/jovi3 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.34
+// @version      3.35
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
@@ -69,8 +69,8 @@ var Comment16 = 'CITA4D*SOTAR6*32*75*95';
 
 /*======================================================================3copy tampermonkey==========================================================*/
 
-var namagroup17 = 'popok';
-var Comment17 = 'wewe';
+var namagroup17 = 'Jawatengah';
+var Comment17 = 'Jivi3 3';
 
 var namagroup18 = 'colt';
 var Comment18 = 'wewe';
@@ -100,7 +100,7 @@ var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
 
-var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah",
+var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah","nabila",
              "boboho","bonar",
              "chikaku","chinostia",
              "denis","debby","diva","dewa",
