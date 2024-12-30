ca415636c7fd3510c97a74ffc9cf3dd85b687e6a
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-30 17:29:17 +0700

    Update jovi3 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- jovi 3/jovi3 3.js
+++ jovi 3/jovi3 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.36
+// @version      3.37
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
@@ -31,8 +31,8 @@ var Comment3 = 'Shiotogel4d (SIKON7) 32*75*95';
 var namagroup4 = 'NEMO';
 var Comment4 = 'Nemo4d (SAVON7) : 11*24*69';
 
-var namagroup5 = '(WARTO)';
-var Comment5 = '‌#(WARUNGTOTO) = (SAPIX6) 00*83*92';
+var namagroup5 = 'LICIN4D';
+var Comment5 = '#LICIN4D (SAPIX6) 00*83*92'; 
 
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (SYMON2) = 04*30*93';
@@ -70,10 +70,10 @@ var Comment16 = 'CITA4D*SOTAR6*32*75*95';
 /*======================================================================3copy tampermonkey==========================================================*/
 
 var namagroup17 = 'Jawatengah';
-var Comment17 = 'Jivi3 3';
+var Comment17 = 'tab 3 line3';
 
 var namagroup18 = 'colt';
-var Comment18 = 'wewe';
+var Comment18 = 'tab 3 line3';
 
 var refresh = 40;
 var d = new Date();
@@ -100,7 +100,7 @@ var id17 = await GM.getValue(17);
 var id18 = await GM.getValue(18);
 
 
-var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah","nabila",
+var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah","nabila","arsyah,
              "boboho","bonar",
              "chikaku","chinostia",
              "denis","debby","diva","dewa",
@@ -121,7 +121,7 @@ var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah",
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
-             "wijaya",
+             "wijaya","widya",
              "yoky","yaya",
              "zion","zhelonk","zimbabwe","zhie"];
 
