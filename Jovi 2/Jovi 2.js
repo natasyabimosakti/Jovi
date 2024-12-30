43944fba38fcb9afa262a25820c73e1a9bc0f253
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2024-12-30 17:33:01 +0700

    Update Jovi 2.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 2.js
+++ Jovi 2/Jovi 2.js
@@ -40,7 +40,7 @@ var Comment6 = '#DIVA4D (TRUKKS4) = 09*29*66';
 var namagroup7 = 'MAYAPADA';
 var Comment7 = 'BETT*SingasariTOTO(BO)*TSUKMA3*02*40*54'; 
 
-var namagroup8 = 'ALLPAS';
+var namagroup8 = '𝐀𝐋𝐋𝐏𝐀𝐒';
 var Comment8 = 'Tok99Toto ( TCUKE2 ) : 13*16*42';
 
 var namagroup9 = 'TOYIBSLOT';
