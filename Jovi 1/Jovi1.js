e9797d3c7fc93f5e974023b3ff30deb2946597a4
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-01 12:41:22 +0700

    Update Jovi1.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 1/Jovi1.js
+++ Jovi 1/Jovi1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.59
+// @version      3.60
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL  	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi1.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi1.js
@@ -62,8 +62,8 @@ var Comment13 = 'SIJITOGEL CKZLT1 29*66*83';
 var namagroup14 = 'BLITAR';
 var Comment14 = '#BLITAR4D ( CEB689B ) : 01*30';
 
-var namagroup15 = 'RNR';
-var Comment15 = '#RNR303(CEYENGG6) : 02*15*68';
+var namagroup15 = 'BENIHTOTO';
+var Comment15 = '#BENIHTOTO(CEYENGG6) : 02*15*68';
 
 var namagroup16 = 'KECERDASAN';
 var Comment16 = 'CITA4D*CYUCUKBERUK*32*42*91';
