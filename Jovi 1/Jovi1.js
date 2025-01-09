3c835986ee6abfd0696ee426e9a232d81f67e77e
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-09 20:24:07 +0700

    Update Jovi1.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 1/Jovi1.js
+++ Jovi 1/Jovi1.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 1
 // @namespace    http://tampermonkey.net/
-// @version      3.61
+// @version      3.62
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL  	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi1.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi1.js
