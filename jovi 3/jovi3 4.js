9cb203338b986c862d172e943a1cba46c3c8c3b1
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-13 20:40:53 +0700

    Update jovi3 4.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- jovi 3/jovi3 4.js
+++ jovi 3/jovi3 4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.46
+// @version      3.47
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%204.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%204.js
@@ -28,7 +28,7 @@ var Comment2 = 'K86TOTO ( TILOR1 ) : 12*18*54';
 var namagroup3 = '(WARTO)';
 var Comment3 = '#(WARUNGTOTO) = (TAVIX6) 15*81*84';
 
-var namagroup4 = 'NEMO';
+var namagroup4 = 'Nemo4d';
 var Comment4 = 'Nemo4d (TIMAX9) : 34*73*85';
 
 var namagroup5 = 'LICIN4D';
@@ -104,7 +104,7 @@ var admin = ["Siâo","ayesha","aqisya","asyati","andinincy","andy","ardiansyah",
              "chikaku","chinostia",
              "denis","debby","diva","dewa",
              "erwin","eryna",
-             "ferlita",
+             "ferlita","fillea",
              "gretha","garda",
              "hokii","hoki","har",
              "inisial","ishaura",
