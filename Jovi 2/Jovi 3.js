7927e397b3789ee4acdf0966b0ddc6330d633dea
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-01-25 19:09:01 +0700

    Update Jovi 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.20
+// @version      3.21
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
@@ -104,7 +104,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "boboho","bonar",
              "chikaku","chinostia",
              "denis","debby","diva","dewa",
-             "erwin","eryna",
+             "erwin","eryna","erlangga",
              "ferlita","fillea",
              "gretha","garda",
              "hokii","hoki","har",
@@ -121,7 +121,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
-             "wijaya","widya",
+             "wijaya","widya","wijaya",
              "yoky","yaya",
              "zion","zhelonk","zimbabwe","zhie"];
 
