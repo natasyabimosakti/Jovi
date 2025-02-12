ec66a5695e05bdecc6c77ede872bf870cba3d922
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-02-12 18:00:47 +0700

    Update Jovi3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 1/Jovi3.js
+++ Jovi 1/Jovi3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.68
+// @version      3.69
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi3.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi3.js
@@ -32,8 +32,8 @@ var Comment3 = '#(WARUNGTOTO) = (BUDJANG3) 27*76*90';
 var namagroup4 = 'Nemo4d';
 var Comment4 = 'Nemo4d (BKL5FCC) : 51*52*69';
 
-var namagroup5 = 'BIDAK';
-var Comment5 = 'BIDAKT0T0 ( BUDJANG3 ) 55*70*74';
+var namagroup5 = 'LIVE';
+var Comment5 = 'SAHARA888 ( BUDJANG3 ) 55*70*74';
 
 var namagroup6 = 'DIVA4D';
 var Comment6 = '#DIVA4D (BADJING30) = 36*56*82';
@@ -116,7 +116,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "manu","mas har","manu roh","mad rm","mad","monica","mersya",
              "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine",
              "oscar","otong",
-             "panambang","pram",
+             "panambang","pram","pengurus",
              "rohmanu","rungkad","riko",
              "sanchez","syaqeena","sundawa","sidebar","sam","saputra","shell","suzanawati",
              "tink","thonex","tiara","tania","tag",
