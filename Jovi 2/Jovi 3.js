3a6b8258f05cc2d998a0e9b1ec18a24e5294c317
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-03-17 16:00:07 +0700

    Update Jovi 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 3.js
+++ Jovi 2/Jovi 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.27
+// @version      3.28
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
 // @downloadURL  https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%203.js
@@ -49,8 +49,8 @@ var Comment9 = '#TOYIBSLOT ( JJWALI5 ) : 04*36*51';
 var namagroup10 = 'LING';
 var Comment10 = 'LING77 (JDJS4) 85*90';
 
-var namagroup11 = 'JOGJATOTO';
-var Comment11 = '#JOGJATOTO (JOKIAD8)= 46-84';
+var namagroup11 = 'TEMPAT';
+var Comment11 = '#AMAVI5D ( JOKIAD8 ) : 46*84*96';
   
 var namagroup12 = 'TARUNG';
 var Comment12 = '#TARUNGTOTO (JELONN9)=38_50';
@@ -117,7 +117,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "oscar","otong",
              "panambang","pram","pengurus",
              "rohmanu","rungkad","riko",
-             "sanchez","syaqeena","sundawa","sidebar","sam","saputra","shell","suzanawati","sahara",
+             "sanchez","syaqeena","sundawa","sidebar","sam","saputra","shell","suzanawati","sahara","sintya",
              "tink","thonex","tiara","tania","tag",
              "ujen",
              "vonny","viona","virna",
