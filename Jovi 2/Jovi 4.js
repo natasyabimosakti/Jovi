5b602891e864890b01c1fc4ce1f603c5b92e4e7b
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-21 00:51:42 +0700

    Update Jovi 4.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 4.js
+++ Jovi 2/Jovi 4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.32
+// @version      3.33
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%204.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%204.js
@@ -52,8 +52,8 @@ var Comment10 = 'LING77 (VNTM57) 28*31';
 var namagroup11 = 'AMAVI5D';
 var Comment11 = '#AMAVI5D ( VGARLA5 ) : 47*79*86';
 
-var namagroup12 = 'TARUNG';
-var Comment12 = '#TARUNGTOTO (VARNOL8)=03_24';
+var namagroup12 = 'KASTOTO';
+var Comment12 = '#KASTOTO(VARNOL8) = 03*24*99 #AGENTOGELTERPERCAYA';
 
 var namagroup13 = 'TAFSIR';
 var Comment13 = 'SIJITOGEL VNKZP4 08*68*74'; 
@@ -112,7 +112,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "jovanka","jenifer",
              "karla","kumbara",
              "langit","leaderbrutallovely","laura","lidya","lusiana",
-             "manu","mas har","manu roh","mad rm","mad","monica","mersya",
+             "manu","mas har","manu roh","mad rm","mad","monica","mersya","maley",
              "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine",
              "oscar","otong",
              "panambang","pram","pengurus",
