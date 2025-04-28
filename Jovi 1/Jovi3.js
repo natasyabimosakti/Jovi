1d72262a1906593de7427180456e84869ff36adc
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-28 10:05:03 +0700

    Update Jovi3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 1/Jovi3.js
+++ Jovi 1/Jovi3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.79
+// @version      3.80
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi3.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%201/Jovi3.js
@@ -47,8 +47,8 @@ var Comment8 = 'Tok99Toto ( BHYPOK7 ) : 46*47*62';
 var namagroup9 = 'TOYIBSLOT';
 var Comment9 = '#TOYIBSLOT ( BJL89KA ) : 55*70*74';
 
-var namagroup10 = 'LING';
-var Comment10 = 'LING77 (ANXIK4) 08*41'; 
+var namagroup10 = 'RIATOTO';
+var Comment10 = '#RIATOTO ANXIK4 = 08*41*88';
 
 var namagroup11 = 'AMAVI5D';
 var Comment11 = '#AMAVI5D ( BPKSTV54 ) : 12*44*45';
@@ -105,7 +105,7 @@ var refresh = 40;
 var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ardiansyah","arsyah","azahra","aurora","anisa",
              "boboho","bonar","Bogard",
              "chikaku","chinostia",
-             "denis","debby","diva","dewa","done",
+             "denis","debby","diva","dewa","done","damara",
              "erwin","eryna","erlangga",
              "ferlita","fillea",
              "gretha","garda","givano",
@@ -115,7 +115,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "karla","kumbara",
              "langit","leaderbrutallovely","laura","lidya","lusiana",
              "manu","mas har","manu roh","mad rm","mad","monica","mersya","maley",
-             "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine",
+             "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine","new9eneration",
              "oscar","otong",
              "panambang","pram","pengurus",
              "rohmanu","rungkad","riko","ratuliu",
