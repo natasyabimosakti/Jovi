7f5ecc11436e6963d4a9476a4049115717a9be17
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-04-28 23:39:26 +0700

    Update Jovi 2.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- Jovi 2/Jovi 2.js
+++ Jovi 2/Jovi 2.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 2
 // @namespace    http://tampermonkey.net/
-// @version      3.34
+// @version      3.35
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%202.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/Jovi%202/Jovi%202.js
@@ -46,8 +46,8 @@ var Comment8 = 'Tok99Toto ( TCUKE2 ) : 13*16*42';
 var namagroup9 = 'TOYIBSLOT';
 var Comment9 = '#TOYIBSLOT ( TWURC3 ) : 13*16*42';
 
-var namagroup10 = 'LING';
-var Comment10 = 'LING77 (TTEXW3) 14*44';
+var namagroup10 = 'RIATOTO';
+var Comment10 = '#RIATOTO TTEXW3 = 14*44*59';
 
 var namagroup11 = 'AMAVI5D';
 var Comment11 = '#AMAVI5D ( THIYOKK7 ) : 02*40*54';
@@ -103,7 +103,7 @@ var id18 = await GM.getValue(18);
 var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ardiansyah","arsyah","azahra","aurora","anisa",
              "boboho","bonar","Bogard",
              "chikaku","chinostia",
-             "denis","debby","diva","dewa","done",
+             "denis","debby","diva","dewa","done","damara",
              "erwin","eryna","erlangga",
              "ferlita","fillea",
              "gretha","garda","givano",
@@ -113,7 +113,7 @@ var admin = ["Siâo","nabila","ayesha","aqisya","asyati","andinincy","andy","ard
              "karla","kumbara",
              "langit","leaderbrutallovely","laura","lidya","lusiana",
              "manu","mas har","manu roh","mad rm","mad","monica","mersya","maley",
-             "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine",
+             "nadila","neng","naura","nathaya","nick","nella","neman","novi","nadhine","new9eneration",
              "oscar","otong",
              "panambang","pram","pengurus",
              "rohmanu","rungkad","riko","ratuliu",
