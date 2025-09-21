5dbf5bc5e35bffc9a1ffbfc90cee84c71ab16bde
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-09-22 02:09:52 +0700

    Update jovi3 4.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- jovi 3/jovi3 4.js
+++ jovi 3/jovi3 4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 4
 // @namespace    http://tampermonkey.net/
-// @version      3.80
+// @version      3.81
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%204.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%204.js
@@ -32,7 +32,7 @@ var namagroup2 = 'K86';
 var Comment2 = 'K86TOTO ( TILOR1 ) : 12*18*54';
 
 var namagroup3 = '(WARTO)';
-var Comment3 = '#(WARUNGTOTO) = (TAVIX6) 15*81*84';
+var Comment3 = '#(WARUNGTOTO) = (TAVIX6) 15*81';
 
 var namagroup4 = 'ASIA';
 var Comment4 = '#ASIATOTO = {TIMAX9} = 34*73'; 
