f31609d9a9660ca8b0bee0490519285d4c1f1c95
natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>
2025-11-07 01:43:48 +0700

    Update jovi3 3.js
    Signed-off-by: natasyabimosakti <158560322+natasyabimosakti@users.noreply.github.com>

--- jovi 3/jovi3 3.js
+++ jovi 3/jovi3 3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JOVI 3
 // @namespace    http://tampermonkey.net/
-// @version      3.83
+// @version      3.84
 // @description  Script Metode Terbaru, Cari Semua Keyword
 // @updateURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
 // @downloadURL	 https://raw.githubusercontent.com/natasyabimosakti/Jovi/refs/heads/main/jovi%203/jovi3%203.js
@@ -55,8 +55,8 @@ var Comment9 = '#TOYIBSLOT ( SIMEX9 ) : 60*77*99';
 var namagroup10 = 'UPINSLOT';
 var Comment10 = '#UPINSLOT ( SAPIX6 ) 11*24*69 BETTING'; 
 
-var namagroup11 = 'MELATI';
-var Comment11 = '*( MELATI188 ) ( SURTI4 ) 33*68 *BETTING *TOGELONLINE';
+var namagroup11 = 'VESPA7093L';
+var Comment11 = 'VESPATOGEL (SURTI4) 33*68*96'; 
 
 var namagroup12 = 'OPUNG';
 var Comment12 = 'OPUNG4D ( SAVON7 ) : 06*22*91';
